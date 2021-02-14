3acf56feda95253cddcde275fb3cb5b7727d5077
Jonathan Henderson <jkhenderson999@gmail.com>
2021-02-13 19:22:51 -0600

    Add deployment script

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+# Abort on errors
+set -e
+
+# Build
+npm run build
+
+# Navigate to build output directory
+cd dist
+
+git init
+git add -A
+git commit -m "deploy"
+git push -f git@github.com:jawbone999/jawbone999-portfolio.git master:gh-pages
+
+cd -
